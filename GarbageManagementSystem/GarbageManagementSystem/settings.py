37428463da026a648903d27aa7e2c64e524fa70e
AromalShaji <92853159+AromalShaji@users.noreply.github.com>
2023-05-26 11:05:51 +0530

    Update settings.py

--- GarbageManagementSystem/GarbageManagementSystem/settings.py
+++ GarbageManagementSystem/GarbageManagementSystem/settings.py
@@ -138,5 +138,5 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'aromal.mca2123@saintgits.org'
-EMAIL_HOST_PASSWORD = 'saintgits@'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'password'
